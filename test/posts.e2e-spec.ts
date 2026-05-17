b0d7f4cc0729ea4dd000b7b97e5cded781f26e71
Alex <steblyakov.aleksandr@gmail.com>
2026-05-17 18:47:06 +0300

    Lesson 14 - preparing repository

--- test/posts.e2e-spec.ts
+++ test/posts.e2e-spec.ts
@@ -43,7 +43,17 @@ describe('PostsController (e2e)', () => {
             .expect(201);
 
         const blog = createBlogResponse.body;
-
+        // структура возвращаемого блога
+        /*
+        {
+        "id": "69f629a4b705ee0b0e4b874e",
+        "name": "NodeJS Blog",
+        "description": "Backend news",
+        "websiteUrl": "https://nodejs.org",
+        "createdAt": "2026-05-02T16:43:16.921Z",
+        "isMembership": true
+        }
+        */
         // создание поста для этого блога
         const createPostDto = {
             title: 'NestJS Testing',
@@ -58,46 +68,37 @@ describe('PostsController (e2e)', () => {
 
         const createdPost = createPostResponse.body;
 
-        /*
-        {
-        "id": "69f629a4b705ee0b0e4b874e",
-        "name": "NodeJS Blog",
-        "description": "Backend news",
-        "websiteUrl": "https://nodejs.org",
-        "createdAt": "2026-05-02T16:43:16.921Z",
-        "isMembership": true
-        }
-         */
+        // закомментированное относится к комментам, они не находятся непосредственно в ответе
+        // const createCommentDto = {
+        //     relatedPostId: createdPost.id,
+        //     content: 'comment',
+        //     commentatorInfo: {
+        //         userId: 'some userId',
+        //         userLogin: 'some login',
+        //     },
+        // };
 
-        const createCommentDto = {
-            relatedPostId: createdPost.id,
-            content: 'comment',
-            commentatorInfo: {
-                userId: 'some userId',
-                userLogin: 'some login',
-            },
-        };
-
-        const createCommentResponse = await request(app.getHttpServer())
-            .post(`/comments/`)
-            .send(createCommentDto)
-            .expect(201);
-
-        // просто дополнительная проверка того, что размещение коммента отработало как надо
-        expect(createCommentResponse.body).toEqual({
-            id: expect.any(String),
-            content: "comment",
-            commentatorInfo: {
-                userId: "some userId",
-                userLogin: "some login"
-            },
-            createdAt: expect.any(String),
-            likesInfo: {
-                likesCount: 0,
-                dislikesCount: 0,
-                myStatus: "None"
-            }
-        });
+        // // создаем коммент в посте createdPost.id
+        // const createCommentResponse = await request(app.getHttpServer())
+        //     .post(`/comments/`)
+        //     .send(createCommentDto)
+        //     .expect(201);
+        //
+        // // просто дополнительная проверка того, что размещение коммента отработало как надо
+        // expect(createCommentResponse.body).toEqual({
+        //     id: expect.any(String),
+        //     content: "comment",
+        //     commentatorInfo: {
+        //         userId: "some userId",
+        //         userLogin: "some login"
+        //     },
+        //     createdAt: expect.any(String),
+        //     likesInfo: {
+        //         likesCount: 0,
+        //         dislikesCount: 0,
+        //         myStatus: "None"
+        //     }
+        // });
         // console.log(createCommentResponse.body);
 
         const result = await request(app.getHttpServer())
@@ -110,25 +111,101 @@ describe('PostsController (e2e)', () => {
             page: 1,
             pageSize: 10,
             items: [
-            {
-                id: expect.any(String),
-                title: "NestJS Testing",
-                shortDescription: "How to write e2e tests",
-                content: "Very long and useful content about supertest...",
-                blogId: expect.any(String),
-                blogName: "NodeJS Blog",
-                createdAt: expect.any(String),
-                extendedLikesInfo: {
-                    likesCount: 0,
-                    dislikesCount: 0,
-                    myStatus: "None",
-                    newestLikes: []
+                {
+                    id: expect.any(String),
+                    title: "NestJS Testing",
+                    shortDescription: "How to write e2e tests",
+                    content: "Very long and useful content about supertest...",
+                    blogId: expect.any(String),
+                    blogName: "NodeJS Blog",
+                    createdAt: expect.any(String),
+                    extendedLikesInfo: {
+                        likesCount: 0,
+                        dislikesCount: 0,
+                        myStatus: "None",
+                        newestLikes: []
+                    }
                 }
-            }
-        ]
+            ]
         });
-
         // console.log(result.body);
 
     });
+
+    it('GET /posts - should return 200 and paginated post list', async () => {
+
+        // создание блога
+        const createBlogResponse = await request(app.getHttpServer())
+            .post('/blogs')
+            .send({
+                name: 'NodeJS Blog',
+                description: 'Backend news',
+                websiteUrl: 'https://nodejs.org',
+            })
+            .expect(201);
+
+        const blog = createBlogResponse.body;
+
+        // структура возвращаемого блога
+        /*
+        {
+        "id": "69f629a4b705ee0b0e4b874e",
+        "name": "NodeJS Blog",
+        "description": "Backend news",
+        "websiteUrl": "https://nodejs.org",
+        "createdAt": "2026-05-02T16:43:16.921Z",
+        "isMembership": true
+        }
+        */
+
+        // создание поста для этого блога
+        const createPostDto = {
+            title: 'NestJS Testing',
+            shortDescription: 'How to write e2e tests',
+            content: 'Very long and useful content about supertest...',
+            blogId: blog.id,
+        };
+
+        const createPostResponse = await request(app.getHttpServer())
+            .post(`/posts`)
+            .send(createPostDto)
+            .expect(201);
+
+
+        // структура ответа с содержанием созданного поста
+        // {
+        //     "id": "6a09e013dc2fce1338ab466a",
+        //     "title": "NestJS Testing",
+        //     "shortDescription": "How to write e2e tests",
+        //     "content": "Very long and useful content about supertest...",
+        //     "blogId": "6a09e012dc2fce1338ab4666",
+        //     "blogName": "NodeJS Blog",
+        //     "createdAt": "2026-05-17T15:34:43.367Z",
+        //     "extendedLikesInfo": {
+        //     "likesCount": 0,
+        //         "dislikesCount": 0,
+        //         "myStatus": "None",
+        //         "newestLikes": []
+        // }
+        // }
+
+        expect(createPostResponse.body).toEqual({
+
+            id: expect.any(String),
+            title: "NestJS Testing",
+            shortDescription: "How to write e2e tests",
+            content: "Very long and useful content about supertest...",
+            blogId: expect.any(String),
+            blogName: "NodeJS Blog",
+            createdAt: expect.any(String),
+            extendedLikesInfo: {
+                likesCount: 0,
+                dislikesCount: 0,
+                myStatus: "None",
+                newestLikes: []
+            }
+
+        });
+    });
+
 });
