f54799fd2a07ac312006254e07c7c8df6f4c6f8d
Alex <steblyakov.aleksandr@gmail.com>
2026-05-26 20:00:49 +0300

    Lesson 14 - ratelimitlog entity schema created as well as compound index

--- src/modules/authorisation/domain/rate-limit-log.entity.ts
+++ src/modules/authorisation/domain/rate-limit-log.entity.ts
@@ -1,9 +1,12 @@
-import {Prop, Schema} from "@nestjs/mongoose";
+import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+import {HydratedDocument, Model} from "mongoose";
+import {CreateRateLimitSessionPayload} from "./payload/create-rate-limit-session.domain.payload";
+
 
 @Schema()
 export class RateLimitLog {
     @Prop({type: String, required: true})
-    deviceIp: string;
+    deviceIP: string;
 
     @Prop({type: String, required: true})
     deviceName: string;
@@ -11,11 +14,34 @@ export class RateLimitLog {
     @Prop({type: String, required: true})
     calledURL: string;
 
-    @Prop({type: Date, required: true})
+    @Prop({type: Date, required: true, expires: '30s'})
     dateOfRequest: Date;
 
     get id() {
         // @ts-ignore
         return this._id.toString();
     }
+
+    static createInstance(sessionPayload: CreateRateLimitSessionPayload): RateLimitLogDocument {
+        const newRateLimitSession = new this();
+
+        newRateLimitSession.deviceIP = sessionPayload.deviceIP;
+        newRateLimitSession.deviceName = sessionPayload.deviceName;
+        newRateLimitSession.calledURL = sessionPayload.calledURL;
+
+        newRateLimitSession.dateOfRequest = new Date();
+
+        return newRateLimitSession as RateLimitLogDocument;
+    }
 }
+
+
+export const RateLimitLogSchema = SchemaFactory.createForClass(RateLimitLog);
+
+RateLimitLogSchema.loadClass(RateLimitLog);
+
+RateLimitLogSchema.index({ deviceIP: 1, deviceName: 1, calledURL: 1 });
+
+export type RateLimitLogDocument = HydratedDocument<RateLimitLog>;
+
+export type RateLimitLogModelType = Model<RateLimitLogDocument> & typeof RateLimitLog;
