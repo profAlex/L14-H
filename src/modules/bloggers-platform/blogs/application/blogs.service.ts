7770622aac56a5d3c088ea49119ac08801f97284
Alex <steblyakov.aleksandr@gmail.com>
2026-05-30 17:42:27 +0300

    Lesson 14 - modifications for tests

--- src/modules/bloggers-platform/blogs/application/blogs.service.ts
+++ src/modules/bloggers-platform/blogs/application/blogs.service.ts
@@ -18,14 +18,33 @@ export class BlogsService {
     }
 
     async createNewBlog(dto: CreateBlogDto): Promise<string> {
+        // const blog = this.BlogModel.createInstance({
+        //     name: dto.name,
+        //     description: dto.description,
+        //     websiteUrl: dto.websiteUrl
+        // });
+        //
+        // await this.blogsCommandRepository.save(blog);
+        //
+        // return blog.id;
+
+        // 🔥 ВЫВОДИМ ТО, ЧТО ПРИШЛО ИЗ КОНТРОЛЛЕРА
+        console.log('=== ДАННЫЕ В СЕРВИСЕ ===', dto);
+
         const blog = this.BlogModel.createInstance({
             name: dto.name,
             description: dto.description,
             websiteUrl: dto.websiteUrl
         });
 
-        await this.blogsCommandRepository.save(blog);
+        // 🔥 ВЫВОДИМ ТО, ЧТО ПОЛУЧИЛОСЬ ПОСЛЕ ФАБРИКИ
+        console.log('=== ОБЪЕКТ ДЛЯ БАЗЫ ===', {
+            name: blog.name,
+            description: blog.description,
+            websiteUrl: blog.websiteUrl
+        });
 
+        await this.blogsCommandRepository.save(blog);
         return blog.id;
     }
 
